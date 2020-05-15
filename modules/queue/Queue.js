0d53fd194ef04ee455960fb435ee79300964fd66
Trina <trinamceary@gmail.com>
2020-05-15 12:42:37 -0700

    complete queue

--- modules/queue/Queue.js
+++ modules/queue/Queue.js
@@ -1,22 +1,58 @@
+class _Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 class Queue {
   constructor() {
-    // Set initial data.
+   this.first = null;
+   this.last = null;
   }
 
-  enqueue(data) {
-    // Add some data to the queue.
+  enqueue(item) {
+    const node = new _Node(item);
+    if (this.first === null) {
+      this.first = node;
+    }
+    if (this.last){
+      this.last.next = node;
+    }
   }
 
   dequeue() {
-    // Remove some data from the queue.
+    if (!this.first) {
+      return;
+    }
+    const node = this.first;
+    this.first = node.next;
+
+    if (node === this.last) {
+      this.last = null;
+    }
+    return node.value
   }
 
   show() {
     // Return the next item in the queue.
+    if (queue.first === null) return;
+    return queue.first.value;
   }
 
   all() {
     // Return all items in the queue.
+    if (queue.first === null) return;
+    let builder = '';
+    let current = queue.first;
+    builder += current.value;
+    while (current.next !== null) {
+      current = current.next;
+      builder += ' -> ' + current.value;
+    }
+    builder += ' -> tail';
+    console.log(builder);
+    return;
   }
 }
 
