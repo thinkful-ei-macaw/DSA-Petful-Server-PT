3aa2c5491192c6814fcb512b9f0692656697c322
Trina <trinamceary@gmail.com>
2020-06-06 19:13:04 -0700

    ppl router

--- modules/people/people.router.js
+++ modules/people/people.router.js
@@ -7,10 +7,17 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
   // Return all the people currently in the queue.
+  return res.json(People.get());
 })
 
 router.post('/', json, (req, res) => {
   // Add a new person to the queue.
+  if (!req.body.name) {
+    res.status(400, 'Name is required')
+  } else {
+    People.enqueue(req.body.name);
+    res.status(201).end();
+  }
 })
 
 module.exports = router
